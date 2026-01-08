666a66632102c558bc3a9a9a1920774487a2cd69
Russell Ferrero <russellferrero00@gmail.com>
2026-01-09 02:35:02 +0800

    feat(auth): add User model and password handling

---
+++ backend/models/User.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name required!'],
+    trim: true,
+    maxlength: 100,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email required!'],
+    trim: true, 
+    lowercase: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: [true, 'Password required!'],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['applicant', 'employer', 'admin'],
+    default: 'applicant',
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now,
+  },
+  lastLoginAt: {
+    type: Date,
+    default: null,
+  }
+}, { timestamps: true });
+
+// Password Hashing
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('passwordHash')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(13);
+  this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+  next();
+})
+
+//Password Comparison
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.passwordHash);
+};
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
