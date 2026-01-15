c10e55e1368a54971cb86b1ebf8431dbd4c9d9e6
Russell Ferrero <russellferrero00@gmail.com>
2026-01-15 22:04:26 +0800

    feat(company-profile): finalize and test all routes and rbac edge cases

--- backend/routes/companyProfile.routes.js
+++ backend/routes/companyProfile.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import requireAuth from '../middlewares/auth.middleware.js';
+import requireRole from '../middlewares/requireRole.middleware.js';
 import { companyLogoUpload } from '../config/companyLogoUpload.config.js';
+
 
 import {
   createProfile,
@@ -12,10 +14,10 @@ import {
 
 const companyProfileRouter = Router();
 
-companyProfileRouter.post('/profile', requireAuth, createProfile);
-companyProfileRouter.get('/profile', requireAuth, getProfile);
-companyProfileRouter.patch('/profile', requireAuth, updateProfile);
-companyProfileRouter.delete('/profile', requireAuth, deleteProfile);
-companyProfileRouter.put('/profile/logo', requireAuth, companyLogoUpload.single('logo'), uploadCompanyLogo);
+companyProfileRouter.post('/profile', requireAuth, requireRole('employer'),createProfile);
+companyProfileRouter.get('/profile', requireAuth, requireRole('employer'), getProfile);
+companyProfileRouter.patch('/profile', requireAuth, requireRole('employer'), updateProfile);
+companyProfileRouter.delete('/profile', requireAuth, requireRole('employer'), deleteProfile);
+companyProfileRouter.put('/profile/logo', requireAuth, requireRole('employer'), companyLogoUpload.single('logo'), uploadCompanyLogo);
 
 export default companyProfileRouter;
